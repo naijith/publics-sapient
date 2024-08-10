394a54ff6a84611d12d1036d2ecae27b70d055dd
naijith <gnaijith78@gmail.com>
2024-08-10 15:16:12 +0530

    setup football-standings-service

---
+++ football-standings-service/src/test/java/com/publicssapients/football/football_standings_service/FootballStandingsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publicssapients.football.football_standings_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballStandingsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
